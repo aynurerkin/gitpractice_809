d1266937c8de33bdd7fc21ffdb643d9572eafef8
aynurerkin <aynurerkin@gmail.com>
2020-08-09 22:43:38 -0700

    git status practice

--- src/login.java
+++ src/login.java
@@ -1,5 +1,7 @@
 public class login {
     public static void main(String[] args) {
         System.out.println("git practice");
+        //adding extra lines for practicing
+        System.out.println("git status practice");
     }
 }
