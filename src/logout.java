112b410f9f24b01e260b45176a70b31c36527b2f
aynurerkin <64187339+aynurerkin@users.noreply.github.com>
2020-08-09 22:57:16 -0700

    practicing git push

--- src/logout.java
+++ src/logout.java
@@ -2,5 +2,6 @@ public class logout {
     public static void main(String[] args) {
         System.out.println("git push practice");
         System.out.println("adding lines to create second push");
+        System.out.println("practicing git pull");
     }
 }
